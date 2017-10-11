a00bba81092e8c8f6027e16b3d2f2d600ab75430
Jerry Hu <jerryhu1@live.nl>
2017-10-11 18:02:14 +0200

    Initial commit

---
+++ FP-Bomberman-Game/src/Main.hs
@@ -0,0 +1,12 @@
+module Main where
+
+import Graphics.Gloss.Interface.IO.Game
+
+main :: IO ()
+main - playIO (InWindow "Counter") (400, 400) (0,0))
+                black            -- Background color
+                10               -- Frames per second
+                initialState     -- Initial state
+                view             -- View function
+                input            -- Event function
+                step             -- Step function
