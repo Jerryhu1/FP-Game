23f8e8119a6695a40773f3931d9f81cc8193c618
Jerry Hu <jerryhu1@live.nl>
2017-10-11 23:07:19 +0200

    MVC model

---
+++ FP-Bomberman-Game/app/Model.hs
@@ -0,0 +1,24 @@
+-- | This module contains the data types
+--   which represent the state of the game
+module Model where
+
+data InfoToShow = ShowNothing
+                | ShowANumber Int
+                | ShowAChar   Char
+
+nO_SECS_BETWEEN_CYCLES :: Float
+nO_SECS_BETWEEN_CYCLES = 5
+
+data GameState = GameState {
+                   infoToShow  :: InfoToShow
+                 , elapsedTime :: Float
+                 , player       :: Player
+                 }
+
+type Player = Player {
+        health :: Int
+        name   :: String
+}
+
+initialState :: GameState
+initialState = GameState ShowNothing 0
