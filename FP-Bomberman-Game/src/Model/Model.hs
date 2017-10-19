8c366590c13fae4868d133f8c8915a326192aa67
Jerry Hu <jerryhu1@live.nl>
2017-10-19 20:12:25 +0200

    Design document update

--- FP-Bomberman-Game/src/Model/Model.hs
+++ FP-Bomberman-Game/src/Model/Model.hs
@@ -15,7 +15,7 @@ module Model.Model where
     initGame = GameState initPlayer $ createGrid 5 
     
     createGrid :: Int -> Grid
-    createGrid n = [Field (x,y) Empty| y <- [0..n], x <- [0..n]]
+    createGrid n = [Field (x,y) Empty| y <- [0..n], x <- [0..n-2]]
     
     --hier een rng om random blokken in het veld te zetten?
 
