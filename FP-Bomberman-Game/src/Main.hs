6cea1e881a3eef4e3b4513f218a09743a8a48337
Jerry Hu <jerryhu1@live.nl>
2017-10-19 18:34:06 +0200

    Drawing grid now works correctly with a grid of 650 px and field of 50px

--- FP-Bomberman-Game/src/Main.hs
+++ FP-Bomberman-Game/src/Main.hs
@@ -7,8 +7,8 @@ import View
 import Graphics.Gloss.Interface.IO.Game
 
 main :: IO ()
-main = playIO (InWindow "Counter" (400, 400) (0, 0)) -- Or FullScreen
-              black            -- Background color
+main = playIO (InWindow "Counter" (800, 800) (0, 0)) -- Or FullScreen
+              white            -- Background color
               10               -- Frames per second
               initGame         -- Initial state
               view             -- View function
